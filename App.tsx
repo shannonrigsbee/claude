2b31aec35c77585051d767da1549c86653490a4b
shannonrigsbee <shannon.rigsbee@msauto.com>
2026-03-24 12:39:57 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Workshop Dashboard</h1>
+      <p>Your dashboard will go here. Check the README for the prompt to get started.</p>
+    </div>
+  );
+}
